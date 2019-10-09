3fadc9d9869133105fb34208206332dcd1e7e7e8
Jerry C <54548815+chiujienwu@users.noreply.github.com>
2019-10-09 09:46:39 -0500

    Add files via upload
    1.  User should be able to perform a search based on the title of a movie, album, or book
    2.  The search results should display the results and the number of matches
    3.  This should be completed in class before you leave

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using NLog;
+using System.Linq;
+
+namespace MovieLINQ
+{
+    class MainClass
+    {
+        // create a class level instance of logger (can be used in methods other than Main)
+        private static NLog.Logger logger = NLog.LogManager.GetCurrentClassLogger();
+
+        public static void Main(string[] args)
+        {
+            logger.Info("Program started");
+
+            string scrubbedFile = FileScrubber.ScrubMovies("../../movies.csv");
+            MovieFile movieFile = new MovieFile(scrubbedFile);
+
+            Console.WriteLine("Enter search criteria Below ");
+            Console.WriteLine("Search by entering title of movie, album or book:");
+            string response = Console.ReadLine();
+
+            // LINQ - Where filter operator & Contains quantifier operator
+            var Movies = movieFile.Movies.Where(m => m.title.Contains(response));
+            // Display Movies List results after operations
+            foreach (Movie m in Movies)
+            {
+                Console.WriteLine(m.Display());
+            }
+            // LINQ - Count aggregation method
+            Console.WriteLine();
+            Console.WriteLine($"There are {Movies.Count()} in your search");
+
+            logger.Info("Program ended");
+        }
+    }
+}
